747f50d68f2ee67b54721a40ffc35fbe8ac8271b
108CZ <148218237+108CZ@users.noreply.github.com>
2024-04-13 13:46:00 +0900

    Update click.js

--- click.js
+++ click.js
@@ -2,14 +2,16 @@ const number = document.querySelector('h1');
 const plusButton = document.querySelector('button');
 
 let count = 0;
+let count2 = 0;
 
 plusButton.addEventListener('click', () => {
      
       if (count > 5) {
     count = 0;
+    count2 ++;
   } else { 
           count ++;
       }
-        number.innerHTML = "こんにちは、" + count + "さん";
+        number.innerHTML = count2 + "ウェカ" + count + "ピポ";
    // number.innerHTML = count;
 });
