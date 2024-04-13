45db8fbf65c51a100df242e0ddac4ddda0c9fe2a
108CZ <148218237+108CZ@users.noreply.github.com>
2024-04-13 12:58:32 +0900

    Update click.js

--- click.js
+++ click.js
@@ -1,19 +1,13 @@
-/*************************/
-/* クリックカウンターのコード */
-/************************/
+/************************************/
+/* ボタンのクリックで数を1ずつ増やすコード */
+/***********************************/
 
 const number = document.querySelector('h1');
-const plusButton = document.querySelector('#increment');
-const resetButton = document.querySelector('#reset');
+const plusButton = document.querySelector('button');
 
 let count = 0;
 
 plusButton.addEventListener('click', () => {
     count ++; 
-    number.innerHTML = count;
-});
-
-resetButton.addEventListener('click', () => {
-    count = 0; 
     number.innerHTML = count;
 });
