b300fabbf8805150c81a9b14fe1a1ea75b362397
108CZ <148218237+108CZ@users.noreply.github.com>
2024-04-13 13:23:06 +0900

    Update click.js

--- click.js
+++ click.js
@@ -1,13 +1,14 @@
-/************************************/
-/* ボタンのクリックで数を1ずつ増やすコード */
-/***********************************/
-
 const number = document.querySelector('h1');
 const plusButton = document.querySelector('button');
 
 let count = 0;
 
 plusButton.addEventListener('click', () => {
-    count ++; 
-    number.innerHTML = count;
+     
+      if (count > 5) {
+    count = 0;
+  } else { 
+          count ++;
+      }
+   number.innerHTML = count;
 });
